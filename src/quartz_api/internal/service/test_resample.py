b29887d374223ef228ce013b296fd1ae88df731f
devsjc <47188100+devsjc@users.noreply.github.com>
2025-11-26 14:42:28 +0000

    chore(repo): Linting

--- src/quartz_api/internal/service/test_resample.py
+++ src/quartz_api/internal/service/test_resample.py
@@ -1,40 +1,43 @@
+import unittest
+
 import pandas as pd
 
 from quartz_api.internal.models import ActualPower
 from quartz_api.internal.service.resample import resample_generation
 
 
-def test_resample_generation_1_period():
+class TestResampleGeneration(unittest.TestCase):
+    def test_resample_generation_1_period(self):
 
-    values = [
-        ActualPower(Time="2021-01-01T00:00:00Z", PowerKW=1.0),
-        ActualPower(Time="2021-01-01T00:08:00Z", PowerKW=2.0),
-    ]
+        values = [
+            ActualPower(Time="2021-01-01T00:00:00Z", PowerKW=1.0),
+            ActualPower(Time="2021-01-01T00:08:00Z", PowerKW=2.0),
+        ]
 
-    values = resample_generation(values, 15)
+        values = resample_generation(values, 15)
 
-    assert len(values) == 1
-    assert values[0].Time == pd.Timestamp("2021-01-01T00:00:00Z")
-    assert values[0].PowerKW == 1.5
+        self.assertEqual(len(values), 1)
+        self.assertEqual(values[0].Time, pd.Timestamp("2021-01-01T00:00:00Z"))
+        self.assertEqual(values[0].PowerKW, 1.5)
 
 
-def test_resample_generation_3_periods_with_gaps():
+    def test_resample_generation_3_periods_with_gaps(self):
 
-    values = [
-        ActualPower(Time="2021-01-01T00:00:00Z", PowerKW=1.0),
-        ActualPower(Time="2021-01-01T00:08:00Z", PowerKW=2.0),
-        ActualPower(Time="2021-01-01T00:22:00Z", PowerKW=3.0),
-        ActualPower(Time="2021-01-01T01:30:00Z", PowerKW=4.0),
-        ActualPower(Time="2021-01-01T01:31:00Z", PowerKW=5.0),
-        ActualPower(Time="2021-01-01T01:32:00Z", PowerKW=6.0),
-    ]
+        values = [
+            ActualPower(Time="2021-01-01T00:00:00Z", PowerKW=1.0),
+            ActualPower(Time="2021-01-01T00:08:00Z", PowerKW=2.0),
+            ActualPower(Time="2021-01-01T00:22:00Z", PowerKW=3.0),
+            ActualPower(Time="2021-01-01T01:30:00Z", PowerKW=4.0),
+            ActualPower(Time="2021-01-01T01:31:00Z", PowerKW=5.0),
+            ActualPower(Time="2021-01-01T01:32:00Z", PowerKW=6.0),
+        ]
 
-    values = resample_generation(values, 15)
+        values = resample_generation(values, 15)
 
-    assert len(values) == 3
-    assert values[0].Time == pd.Timestamp("2021-01-01T00:00:00Z")
-    assert values[0].PowerKW == 1.5
-    assert values[1].Time == pd.Timestamp("2021-01-01T00:15:00Z")
-    assert values[1].PowerKW == 3.0
-    assert values[2].Time == pd.Timestamp("2021-01-01T01:30:00Z")
-    assert values[2].PowerKW == 5.0
+        self.assertEqual(len(values), 3)
+        self.assertEqual(values[0].Time, pd.Timestamp("2021-01-01T00:00:00Z"))
+        self.assertEqual(values[0].PowerKW, 1.5)
+        self.assertEqual(values[1].Time, pd.Timestamp("2021-01-01T00:15:00Z"))
+        self.assertEqual(values[1].PowerKW, 3.0)
+        self.assertEqual(values[2].Time, pd.Timestamp("2021-01-01T01:30:00Z"))
+        self.assertEqual(values[2].PowerKW, 5.0)
