d196d1985d71831d54d13014fe0983e817f9f4fe
Peter Dudfield <peter.dudfield@hotmail.com>
2026-01-09 13:51:49 +0000

    change window to -2day, floored 2 6 hours

--- src/quartz_api/internal/backends/utils.py
+++ src/quartz_api/internal/backends/utils.py
@@ -1,23 +1,41 @@
 """Utility functions..."""
 
-import datetime as dt
+from datetime import UTC, datetime, timedelta
+
+import numpy as np
 
 
 def get_window(
-    start: dt.datetime | None = None, end: dt.datetime | None = None,
-) -> tuple[dt.datetime, dt.datetime]:
+    start: datetime | None = None, end:datetime | None = None,
+) -> tuple[datetime, datetime]:
     """Returns the start and end of the window for timeseries data."""
     # Window start is the beginning of the day two days ago
     if start is None:
-        start = (dt.datetime.now(tz=dt.UTC) - dt.timedelta(days=2)).replace(
-            hour=0,
-            minute=0,
-            second=0,
-            microsecond=0,
-        )
+        start = (datetime.now(tz=UTC) - timedelta(days=2))
+        start = floor_6_hours_dt(start)
 
     # Window end is the beginning of the day two days ahead
     if end is None:
-        end = start + dt.timedelta(days=4)
+        end = start + timedelta(days=4)
 
     return (start, end)
+
+
+def floor_6_hours_dt(dt: datetime) -> datetime:
+    """Floor a datetime by 6 hours.
+
+    For example:
+    2021-01-01 17:01:01 --> 2021-01-01 12:00:00
+    2021-01-01 19:35:01 --> 2021-01-01 18:00:00
+
+    :param dt: datetime
+    :return: datetime rounded to lowest 6 hours
+    """
+    approx = np.floor(dt.hour / 6.0) * 6.0
+    dt = dt.replace(hour=0)
+    dt = dt.replace(minute=0)
+    dt = dt.replace(second=0)
+    dt = dt.replace(microsecond=0)
+    dt += timedelta(hours=approx)
+
+    return dt
