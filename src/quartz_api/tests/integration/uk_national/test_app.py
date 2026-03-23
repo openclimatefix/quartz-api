34315491ab0c327ad0a5fb429153bf9e8c4cb0a3
katyalmohit <128162892+katyalmohit@users.noreply.github.com>
2026-03-23 17:42:30 +0530

    Resolve conflicts

--- src/quartz_api/tests/integration/uk_national/test_app.py
+++ src/quartz_api/tests/integration/uk_national/test_app.py
@@ -244,7 +244,7 @@ async def test_gsp_pvlive_all(
     assert len(data) == 1
 
     # Calling with specified gsps returns all timestamps for those gsps
-    response = await api_client.get("/v0/solar/GB/gsp/pvlive/all?gsp_ids=1,2,3")
+    response = await api_client_uk_national.get("/v0/solar/GB/gsp/pvlive/all?gsp_ids=1,2,3")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
