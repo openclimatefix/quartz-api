dc7af1636f0d1283b56bd9463f147fd0e2905b8c
Peter Dudfield <peter.dudfield@hotmail.com>
2026-03-24 12:48:08 +0000

    PR comment

--- src/quartz_api/tests/integration/uk_national/test_app.py
+++ src/quartz_api/tests/integration/uk_national/test_app.py
@@ -89,19 +89,19 @@ async def test_national_forecast_metadata_true_and_false(
     data = response.json()
 
     url = f"/v0/solar/GB/national/forecast?include_metadata=true&start_datetime_utc={now}"
-    response = await api_client_uk_national.get(url)
+    response_with_metadata = await api_client_uk_national.get(url)
 
-    assert response.status_code == 200
-    data_metadata = response.json()
+    assert response_with_metadata.status_code == 200
+    data_with_metadata = response.json()
 
-    assert len(data_metadata["forecastValues"]) == len(data)
+    assert len(data_with_metadata["forecastValues"]) == len(data)
     for i in range(10):
-        assert data[i]["targetTime"] == data_metadata["forecastValues"][i]["targetTime"]
+        assert data[i]["targetTime"] == data_with_metadata["forecastValues"][i]["targetTime"]
         assert (
             data[i]["expectedPowerGenerationMegawatts"]
-            == data_metadata["forecastValues"][i]["expectedPowerGenerationMegawatts"]
+            == data_with_metadata["forecastValues"][i]["expectedPowerGenerationMegawatts"]
         )
-    assert data_metadata["model"]["version"] == "1.2.3"
+    assert data_with_metadata["model"]["version"] == "1.2.3"
 
 
 # 3.1 Test the National PVlive route
