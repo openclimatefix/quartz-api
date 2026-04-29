bbb9b13d0dd82f0e8f81df2148079ccd88f26ff4
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2026-04-29 14:53:20 +0100

    make auth more robust, if not email key (#303)
    * make auth more robust, if not email key
    
    * add warning

--- src/quartz_api/internal/middleware/auth.py
+++ src/quartz_api/internal/middleware/auth.py
@@ -83,8 +83,11 @@ class AuthClient:
                 raise e
 
             # set the apitally consumer, this means we can see who has used which routes
-            email = claims["https://openclimatefix.org/email"]
-            set_consumer(request,identifier=email)
+            if EMAIL_KEY in claims:
+                email = claims[EMAIL_KEY]
+                set_consumer(request, identifier=email)
+            else:
+                log.warning("Authentication passed but could not find email claim in JWT")
 
             return claims
 
