11b341de7a723a6edaa4e5e9bb59728253cfdcb1
braddf <info@bradfulford.co.uk>
2026-04-30 15:05:20 +0100

    refactor: rename Capability routes to Discovery

--- src/quartz_api/internal/service/v1/router.py
+++ src/quartz_api/internal/service/v1/router.py
@@ -2,13 +2,13 @@
 
 from fastapi import APIRouter
 
-from .routes.capability import router as capability_router
+from .routes.discovery import router as discovery_router
 from .routes.forecasts import router as forecasts_router
 from .routes.generation import router as generation_router
 from .routes.regions import router as regions_router
 
 router = APIRouter()
-router.include_router(capability_router)
+router.include_router(discovery_router)
 router.include_router(regions_router)
 router.include_router(forecasts_router)
 router.include_router(generation_router)
