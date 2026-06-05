338bf22ef8eeb1da1fee7842085457ba4139a013
suvanbanerjee <banerjeesuvan@gmail.com>
2026-06-05 11:48:59 +0530

    use fsspec instead of boto3

--- src/quartz_api/internal/s3.py
+++ src/quartz_api/internal/s3.py
@@ -1,40 +1,21 @@
 """S3 client."""
-import boto3
-from botocore.client import Config
-from botocore.exceptions import ClientError
+import fsspec
 
 
 class S3Client:
     """S3 client."""
 
     def __init__(self, region_name: str) -> None:
-        """Initialise S3 client.
-
-        :param region_name: The AWS region name.
-        """
-        self.client = boto3.client(
-            "s3",
-            region_name=region_name,
-            config=Config(signature_version="s3v4"),
-        )
+        """Initialise S3 client."""
+        self.fs = fsspec.filesystem("s3", client_kwargs={"region_name": region_name})
 
     def get_presigned_url(self, bucket: str, key: str) -> str:
         """Get a pre-signed URL for a file."""
-        return self.client.generate_presigned_url(
-            "get_object",
-            Params={"Bucket": bucket, "Key": key},
-            ExpiresIn=3600,
-        )
+        return self.fs.sign(f"s3://{bucket}/{key}", expiration=3600)
 
     def object_exists(self, bucket: str, key: str) -> bool:
         """Check if an object exists in S3."""
-        try:
-            self.client.head_object(Bucket=bucket, Key=key)
-            return True
-        except ClientError as e:
-            if e.response["Error"]["Code"] == "404":
-                return False
-            raise
+        return self.fs.exists(f"s3://{bucket}/{key}")
 
 
 _s3_client: S3Client | None = None
