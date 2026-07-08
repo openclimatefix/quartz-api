f5d26f968bbf68fcdce56671d55e6d302fe955ca
Pavan Kulkarni <pavankulkarni369@gmail.com>
2026-07-08 10:18:38 +0530

    fix: lint

--- src/quartz_api/internal/middleware/test_auth.py
+++ src/quartz_api/internal/middleware/test_auth.py
@@ -1,25 +1,26 @@
 from quartz_api.internal.middleware.auth import get_org_id_from_authdata
 
+
 def test_get_org_id_from_authdata_regular_user() -> None:
     authdata = {
         "app_metadata": {
-            "hubspot_company_id": "12345"
+            "hubspot_company_id": "12345",
         },
-        "permissions": ["read:india"]
+        "permissions": ["read:india"],
     }
     assert get_org_id_from_authdata(authdata) == "12345"
 
 def test_get_org_id_from_authdata_admin_user() -> None:
     authdata = {
         "app_metadata": {
-            "hubspot_company_id": "99999999999"
+            "hubspot_company_id": "99999999999",
         },
-        "permissions": ["ocf:admin", "read:india"]
+        "permissions": ["ocf:admin", "read:india"],
     }
     assert get_org_id_from_authdata(authdata) is None
 
 def test_get_org_id_from_authdata_no_metadata() -> None:
     authdata = {
-        "permissions": ["read:india"]
+        "permissions": ["read:india"],
     }
     assert get_org_id_from_authdata(authdata) is None
